fc2be65e2b95a607315c5016fb34b80c426d7185
mandeep <minturajmdb1999@gmail.com>
2024-03-09 23:19:35 +0530

    usereducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useReducer} from 'react'
+
+ function reducer(state,action)
+ {
+   
+   
+    if(action.type==="Increment")
+    {
+       state=Number(state + 1);
+        return state;
+    }
+    else if(action.type==="Decrement")
+    {
+       
+      return Number(state - 1);
+     
+    }
+ }
+
+ let initialvalue={
+  count:0,
+
+ }
 
 function App() {
-  return (
+        
+       
+    const[count,dispatch] = useReducer(reducer,initialvalue.count)
+
+  function handleIncrement()
+  {
+      dispatch({type:"Increment",name:"mandeep"});
+  }
+  function handleDecrement()
+  {
+     dispatch({type:"Decrement"});
+  }
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <label>{count}</label>
+       <div>
+       <button onClick={handleIncrement}>Incremet</button>
+       <button onClick={handleDecrement}>Decrement</button>
+       </div>
     </div>
   );
 }
